30fc305b98d7b94653c5f24d0505659d927f0e21
Ayşe Sude Kara <ayse.kara@std.bogazici.edu.tr>
2024-12-09 23:22:10 +0300

    AyseSudeKARA

--- MyContacts/Views/EditContactPage.xaml.cs
+++ MyContacts/Views/EditContactPage.xaml.cs
@@ -1,25 +1,54 @@
+using Microsoft.Maui.ApplicationModel.Communication;
 using MyContacts.Model;
 
-namespace MyContacts.Views;
-
-
-
-[QueryProperty(nameof(id), "id")]
-public partial class EditContactPage : ContentPage
+namespace MyContacts.Views
 {
-    ContactsRepository repository = new ContactsRepository();
-
-    public string id { get; set; }
-	public EditContactPage()
-	{
-        
-		InitializeComponent();
-	}
-    protected override void OnAppearing()
+    [QueryProperty(nameof(id), "id")]
+    public partial class EditContactPage : ContentPage
     {
-        base.OnAppearing();
-        
-        var contact = repository.GetContact(Int32.Parse(id));
-        selectedContact.Text = id + " " + contact.NameSurname;
+        ContactsRepository contactRepository = new ContactsRepository();
+
+        private ContactInfo contactInfo;
+
+        public string id { get; set; }
+
+        public EditContactPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            contactInfo = contactRepository.GetContact(Int32.Parse(id));
+
+            if (contactInfo != null)
+            {
+                NameEntry.Text = contactInfo.NameSurname;
+                PhoneEntry.Text = contactInfo.PhoneNumber;
+                EmailEntry.Text = contactInfo.Email;
+            }
+            else
+            {
+                DisplayAlert("Hata", "Kiþi bulunamadý", "Tamam");
+            }
+        }
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            contactInfo.NameSurname = NameEntry.Text;
+            contactInfo.PhoneNumber = PhoneEntry.Text;
+            contactInfo.Email = EmailEntry.Text;
+
+            await contactRepository.Update(contactInfo);
+
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//ContactsPage");
+        }
     }
 }
