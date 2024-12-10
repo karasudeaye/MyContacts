0f0c480eb39003d7f6b1cec233b86cbe007df63f
Ayşe Sude Kara <ayse.kara@std.bogazici.edu.tr>
2024-12-10 13:49:51 +0300

    Ayşe Sude Kara

--- MyContacts/Model/ContactsRepository.cs
+++ MyContacts/Model/ContactsRepository.cs
@@ -1,27 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MyContacts.Model
 {
     public class ContactsRepository
     {
-         static List<ContactInfo> contacts = new List<ContactInfo>() { 
-                    new ContactInfo {Id=1, NameSurname="Hüseyin Şimşek", Email="huseyinsimsek@gmail", PhoneNumber="053357252"}
-                    };
+        static List<ContactInfo> contacts = new List<ContactInfo>
+        {
+            new ContactInfo {Id = 1, NameSurname = "Hüseyin Şimşek", Email = "huseyinsimsek@gmail.com", PhoneNumber = "053357252"}
+        };
         static int maxId = 2;
+
         public ContactsRepository()
         {
-            
         }
 
-        public ObservableCollection<ContactInfo> GetContacts() {
-            return new ObservableCollection<ContactInfo>( contacts);
-
+        public ObservableCollection<ContactInfo> GetContacts()
+        {
+            return new ObservableCollection<ContactInfo>(contacts);
         }
 
         public void AddContact(ContactInfo contact)
@@ -32,8 +31,24 @@ namespace MyContacts.Model
 
         public ContactInfo GetContact(int id)
         {
-            return contacts.FirstOrDefault( c => c.Id==id ) ;
+            return contacts.FirstOrDefault(c => c.Id == id);
         }
 
+        public async Task Update(ContactInfo updatedContact)
+        {
+            var existingContact = contacts.FirstOrDefault(c => c.Id == updatedContact.Id);
+            if (existingContact != null)
+            {
+                existingContact.NameSurname = updatedContact.NameSurname;
+                existingContact.Email = updatedContact.Email;
+                existingContact.PhoneNumber = updatedContact.PhoneNumber;
+            }
+            else
+            {
+                throw new Exception("Contact not found.");
+            }
+
+            await Task.CompletedTask;
+        }
     }
 }
